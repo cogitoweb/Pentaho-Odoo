5c1034259e52ae59ddac078dfa2667702d21a800
2zx <paolo.cazzitti@cogitoweb.it>
2020-04-03 17:49:20 +0200

    test

--- odoo_addon/pentaho_reports/static/src/js/ir_actions_report_pentaho_noclose.js
+++ odoo_addon/pentaho_reports/static/src/js/ir_actions_report_pentaho_noclose.js
@@ -4,84 +4,46 @@ odoo.define('pentaho_report.report', function (require) {
   var ActionManager = require('web.ActionManager');
   var framework = require('web.framework');
 
-
-  var trigger_download = function (session, response, c, action, options) {
-    session.get_file({
-        url: '/report/download',
-        data: {data: JSON.stringify(response)},
-        complete: framework.unblockUI,
-        error: c.rpc_error.bind(c),
-        success: function () {
-            if (action && options && !action.dialog) {
-                options.on_close();
-            }
-        },
-    });
-};
-
   ActionManager.include({
-    ir_actions_report_xml_pentaho_noclose: function (action, options) {
+    ir_actions_report_xml_pentaho_noclose: function(action, options) {
       var self = this;
+      framework.blockUI();
       action = _.clone(action);
+      var eval_contexts = ([session.user_context] || []).concat([action.context]);
+      action.context = pyeval.eval('contexts',eval_contexts);
 
-      var report_urls = make_report_url(action);
-
-      if (action.report_type === 'qweb-html') {
-          var client_action_options = _.extend({}, options, {
-              report_url: report_urls['qweb-html'],
-              report_name: action.report_name,
-              report_file: action.report_file,
-              data: action.data,
-              context: action.context,
-              name: action.name,
-              display_name: action.display_name,
-          });
-          return this.do_action('report.client_action', client_action_options);
-      } else if (action.report_type === 'qweb-pdf') {
-          framework.blockUI();
-          // Before doing anything, we check the state of wkhtmltopdf on the server.
-          (wkhtmltopdf_state = wkhtmltopdf_state || session.rpc('/report/check_wkhtmltopdf')).then(function (state) {
-              // Display a notification to the user according to wkhtmltopdf's state.
-              if (WKHTMLTOPDF_MESSAGES[state]) {
-                  self.do_notify(_t('Report'), WKHTMLTOPDF_MESSAGES[state], true);
-              }
-
-              if (state === 'upgrade' || state === 'ok') {
-                  // Trigger the download of the PDF report.
-                  var response = [
-                      report_urls['qweb-pdf'],
-                      action.report_type,
-                  ];
-                  var c = crash_manager;
-                  return trigger_download(self.session, response, c, action, options);
-              } else {
-                  // Open the report in the client action if generating the PDF is not possible.
-                  var client_action_options = _.extend({}, options, {
-                      report_url: report_urls['qweb-html'],
-                      report_name: action.report_name,
-                      report_file: action.report_file,
-                      data: action.data,
-                      context: action.context,
-                      name: action.name,
-                      display_name: action.display_name,
-                  });
-                  framework.unblockUI();
-                  return self.do_action('report.client_action', client_action_options);
-              }
-          });
-      } else if (action.report_type === 'controller') {
-          framework.blockUI();
-          var response = [
-              report_urls.controller,
-              action.report_type,
-          ];
-          var c = crash_manager;
-          return trigger_download(self.session, response, c, action, options);
-      } else {
-          return self._super(action, options);
+      // iOS devices doesn't allow iframe use the way we do it,
+      // opening a new window seems the best way to workaround
+      if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
+          var params = {
+              action: JSON.stringify(action),
+              token: new Date().getTime()
+          };
+          var url = self.session.url('/web/report', params);
+          framework.unblockUI();
+          $('<a href="'+url+'" target="_blank"></a>')[0].click();
+          return;
       }
+      var c = crash_manager;
+      return $.Deferred(function (d) {
+          self.session.get_file({
+              url: '/web/report',
+              data: {action: JSON.stringify(action)},
+              complete: framework.unblockUI,
+              success: function(){
+                  if (!self.dialog) {
+                      options.on_close();
+                  }
+                  /* self.dialog_stop(); */
+                  d.resolve();
+              },
+              error: function () {
+                  c.rpc_error.apply(c, arguments);
+                  d.reject();
+              }
+          });
+      });
   }
 });
-
 
 });
